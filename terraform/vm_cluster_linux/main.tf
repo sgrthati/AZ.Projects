f1433fc43a86fee66a0ce08c558590144c7f2f38
sgrthati <sgrthati@gmail.com>
2024-04-30 19:28:32 +0530

    done

--- terraform/vm_cluster_linux/main.tf
+++ terraform/vm_cluster_linux/main.tf
@@ -1,9 +1,9 @@
-
+#module to create linux vm cluster,based on requirement you can disable by changing below variables
 module "ProjectX" {
     source = "git::https://github.com/sgrthati/AZ.Projects.git//terraform/modules/generic_resources/linux_vm?ref=main"
     resource_group_name = var.resource_group_name
     node_count = 2
-    lb_enabled = false
+    lb_enabled = true
     vm_pip_enabled = true
     dns_enabled = true
     dns_name = "Internal.com"
